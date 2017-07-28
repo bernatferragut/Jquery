a0fb123e86481553b0e4ca639377dcecb68b3acf
Bernat Ferragut <elbernat@gmail.com>
2017-07-28 08:38:55 -0400

    Classic Prevent by Default use cases

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # JQUERY in depth
-
+The Internet DOM manipulation Basics used by 90% of the WWW => A classic necessary to Master
 > Jquery step by step well explained
 
--- main.js
+++ main.js
@@ -11,18 +11,36 @@
 // });
 
 // Accessing am especific KEY
+// $(document).ready(function() {
+//     // $elA input
+//     $elA = $('#enter');
+//     // $elB output
+//     $elB = $('.contenido').children('h1');
+//     //JQ Reaction
+//     $elA.on('keyup', function(event) {
+//         //console.log(event.which) => JS gives 
+//         let eventCode = event.which;
+//         if( eventCode === 13) {
+//             $elB.text($elA.val());
+//             $elA.val('');
+//         }
+//     });
+// });
+
+// PREVENT DEFAULT
 $(document).ready(function() {
-    // $elA input
-    $elA = $('#enter');
-    // $elB output
-    $elB = $('.contenido').children('h1');
-    //JQ Reaction
-    $elA.on('keyup', function(event) {
-        //console.log(event.which) => JS gives 
-        let eventCode = event.which;
-        if( eventCode === 13) {
-            $elB.text($elA.val());
-            $elA.val('');
-        }
+    $formA = $('form');
+    //console.log($formA);
+    // with FORMS
+    $formA.on('submit', function(evt) {
+        evt.preventDefault();
+        console.log('submited!');
+    });
+
+    //with HYPERLINKS
+    $link = $('a');
+    $link.on('click', function(evt) {
+        evt.preventDefault();
+        console.log('clicked!');
     });
 });
